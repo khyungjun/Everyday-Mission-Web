cec21c115e193d92fb1cd6a11c9c2a1ca1f4369f
khyungjun <kpos7503@naver.com>
2023-08-30 17:21:45 +0900

    Init

---
+++ src/test/java/com/everydaymission/api/springboot/EverydayMissionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everydaymission.api.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayMissionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
